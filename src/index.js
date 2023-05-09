485cf8f6960905ad69f5b7540958a31c1f99fd78
Ihor Bodianchuk <ihordobruy@gmail.com>
2023-05-09 23:09:43 +0300

    fix url

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="ingvar789/goit-tweeter-01">
+    <BrowserRouter basename="/goit-tweeter-01">
       <App />
     </BrowserRouter>
   </React.StrictMode>
